968970c14d27eff56bd474919ab185592790ba92
Nolan Farley <nolanfarleytech@gmail.com>
2025-05-29 22:56:45 -0700

    Update main.go (#32)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,7 +20,7 @@ func main() {
 	// Load .env file, ignoring any errors
 	_ = godotenv.Load()
 
-	log.Print("\n V3v3v3v3v3 \n ")
+	log.Print("\n V4v4v4v4v4 \n ")
 
 	// Check APP_ENV after potentially loading it from .env
 	fmt.Print("App Environment Configuration: ")
