15acdee886c35f99e7a6caf237af7419e2aac379
Nolan Farley <nolanfarleytech@gmail.com>
2025-05-31 18:56:43 -0700

    Test 3- CI/CD

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,7 +20,7 @@ func main() {
 	// Load .env file, ignoring any errors
 	_ = godotenv.Load()
 
-	log.Print("\n   ------ VERSION: 1 ------   \n ")
+	log.Print("\n   ------ VERSION: 2 ------   \n ")
 
 	// Check APP_ENV after potentially loading it from .env
 	fmt.Print("App Environment Configuration: ")
